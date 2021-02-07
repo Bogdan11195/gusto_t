725b0942cd79d5b2d39e4eb0e0056f89cb01fabb
Bogdan11195 <pika4y1001@gmail.com>
2021-02-07 23:09:00 +0200

    fc_gusto

--- gusto/settings.py
+++ gusto/settings.py
@@ -120,4 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
