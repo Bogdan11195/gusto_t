60ce2e5823a227947b3a841f77921d18be8f0abe
Bogdan11195 <pika4y1001@gmail.com>
2021-02-08 22:43:07 +0200

    fc_gusto

--- main_gusto/models.py
+++ main_gusto/models.py
@@ -8,14 +8,17 @@ class Category(models.Model):
     category_order = models.IntegerField()
     is_visible = models.BooleanField(default=True)
     is_special = models.BooleanField(default=False)
+
     def __str__(self):
         return f'{self.title} : {self.category_order}'
 
 class Dish(models.Model):
+
     def get_file_name_dishes(self, filename):
         ext = filename.split('.')[-1]
         filename = f'{uuid4()}.{ext}'
         return os.path.join('images/dishes/', filename)
+
     title = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     is_visible = models.BooleanField(default=True)
@@ -23,3 +26,33 @@ class Dish(models.Model):
     photo = models.ImageField(upload_to=get_file_name_dishes)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+
+class Event(models.Model):
+
+    def get_file_name_events(self, filename):
+        ext = filename.split('.')[-1]
+        filename = f'{uuid4()}.{ext}'
+        return os.path.join('images/events/', filename)
+
+    title = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to=get_file_name_events)
+    description = models.TextField(null=True)
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+class Banners(models.Model):
+
+    def get_file_name_banners(self, filename):
+        ext = filename.split('.')[-1]
+        filename = f'{uuid4()}.{ext}'
+        return os.path.join('images/banners/', filename)
+
+    title = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to=get_file_name_banners)
+    is_visible = models.BooleanField(default=True)
+
+
+def __str__(self):
+    return f'{self.title}'
